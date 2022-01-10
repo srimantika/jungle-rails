9996e7b2e6961bb5e89d2384b54780d52c8c8e0b
srimantika <srimantika@gmail.com>
2022-01-10 19:10:43 +0000

    User details

---
+++ spec/user_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context 'password match' do
+    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
+    user.valid?
+    it 'password match' do
+      expect(user.errors[:password_confirmation]).not_to be_present
+    end
+  end
+
+  context "password don't match" do
+    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hoge')
+    user.valid?
+    it "password don't match" do
+      expect(user.errors[:password_confirmation]).to be_present
+    end
+  end
+
+  it 'email must be unique' do
+    user = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
+    user.save
+
+    u = User.new(email: 'test@test.com', password: 'hogehoge', password_confirmation: 'hogehoge')
+    u.save
+
+    expect(u.errors[:email].first).to eq('has already been taken')
+  end
+
+  it 'password length less than 5 characters is invalid' do
+    user = User.new(email: 'bugs', password: '1234')
+
+    result = user.save
+
+    expect(result).to be(false)
+  end
+
+  it 'password length must be at-least 5 characters' do
+    user = User.new(email: 'bugs', password: '12345')
+
+    result = user.save
+
+    expect(result).to be(true)
+  end
+end
